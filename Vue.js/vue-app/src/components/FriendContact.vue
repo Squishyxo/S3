c8ca1a42fcfc4398d80e50172af811be7937b1de
Mohammed Alshukaili <m.aalshukaili@student.fontys.nl>
2022-04-12 14:17:04 +0400

    vue app added

---
+++ Vue.js/vue-app/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+    <template>
+        <li>
+            <h2>{{friend.name}}</h2>
+            <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+            <ul v-if="detailsAreVisible">
+                <li><strong>Phone:</strong>{{friend.phone}}</li>
+                <li><strong>Email:</strong>{{friend.email}}</li>
+            </ul>
+        </li>
+    </template>
+
+<script>
+        export default {
+            data() {
+                return{
+                    detailsAreVisible: false,
+                    friend:  {
+                    id: 1,
+                    name: "Nasser",
+                    age: 49,
+                    email: "Hello@gmail.com"
+                },
+                };
+            },
+            methods: {
+                toggleDetails() {
+                    this.detailsAreVisible = !this.detailsAreVisible;
+                }
+            }
+        };
+</script>
